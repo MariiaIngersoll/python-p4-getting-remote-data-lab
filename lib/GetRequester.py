018c8450f94dc0bf6cdc29ac1c671c0a5900854b
+ <+>
2023-08-02 21:08:11 -0400

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,15 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = requests.get(URL)
+        return response.content
+    
 
     def load_json(self):
-        pass
+        list_of_names = []
+        names_data = json.loads(self.get_response_body())
+        for person in names_data:
+            list_of_names.append(person)
+
+        return list_of_names
